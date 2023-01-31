4256db78d0219025757138902730fd1f44244ee9
Игорь Зацарин <dreadnought26@gmail.com>
2023-01-31 21:04:19 +0300

    #1-#2-#3-Созданы приложения, модели для произведений, категорий, жанров, урлы.

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    pass
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    pass
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    pass
